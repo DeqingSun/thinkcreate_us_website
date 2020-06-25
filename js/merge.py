4ac9b486d6bd58b1f3f383934f4ebc52f0ced7ce
Deqing Sun <iamseer@gmail.com>
2020-06-25 17:45:41 -0400

    merge js

--- js/merge.py
+++ js/merge.py
@@ -1,4 +1,4 @@
-fileNameStr = "jquery-1.11.1.min.js jquery.easing.1.3.js bootstrap.min.js SmoothScroll.js jquery.scrollTo.min.js jquery.localScroll.min.js jquery.viewport.mini.js jquery.countTo.js jquery.appear.js jquery.sticky.js jquery.parallax-1.1.3.js jquery.fitvids.js owl.carousel.min.js isotope.pkgd.min.js imagesloaded.pkgd.min.js jquery.magnific-popup.min.js gmap3.min.js wow.min.js masonry.pkgd.min.js jquery.simple-text-rotator.min.js all.js contact-form.js"
+fileNameStr = "jquery-1.11.1.min.js jquery.easing.1.3.js bootstrap.min.js SmoothScroll.js jquery.scrollTo.min.js jquery.localScroll.min.js jquery.viewport.mini.js jquery.countTo.js jquery.appear.js jquery.sticky.js jquery.parallax-1.1.3.js jquery.fitvids.js owl.carousel.min.js isotope.pkgd.min.js imagesloaded.pkgd.min.js jquery.magnific-popup.min.js gmap3.min.js wow.min.js masonry.pkgd.min.js jquery.simple-text-rotator.min.js rev-slider.js all.js contact-form.js"
 filenames = fileNameStr.split(' ')
 print(filenames)
 
